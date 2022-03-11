e54da202cf56cbf49484b08b772d2e902f567375
MEHAMMEDAMINE <48122920+MEHAMMEDAMINE@users.noreply.github.com>
2022-03-11 19:50:58 +0100

    Update train.py

--- train.py
+++ train.py
@@ -186,7 +186,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', default='bert_spc', type=str)
     parser.add_argument('--dataset', default='laptop', type=str, help='twitter, restaurant, laptop')
-    parser.add_argument('--optimizer', default='adam', type=str)
+    parser.add_argument('--optimizer', default='adadelta', type=str)
     parser.add_argument('--initializer', default='xavier_uniform_', type=str)
     parser.add_argument('--lr', default=2e-5, type=float, help='try 5e-5, 2e-5 for BERT, 1e-3 for others')
     parser.add_argument('--dropout', default=0.1, type=float)
